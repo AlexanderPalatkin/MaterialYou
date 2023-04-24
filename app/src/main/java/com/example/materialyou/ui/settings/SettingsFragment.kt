874bba93259c5d5b494e7769b0c05d7300472051
AlexanderPalatkin <palatkin.alexander@gmail.com>
2023-04-25 02:30:33 +0400

    Урок 2. Добавил возможность смены тем в Settings

--- app/src/main/java/com/example/materialyou/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/example/materialyou/ui/settings/SettingsFragment.kt
@@ -1,10 +1,12 @@
 package com.example.materialyou.ui.settings
 
+import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.view.*
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import coil.load
@@ -17,6 +19,18 @@ import com.example.materialyou.utils.WIKI_URL
 import java.time.LocalDate
 
 class SettingsFragment : Fragment() {
+
+    private val KEY_SP_LOCAL = "sp_local"
+    private val KEY_CURRENT_THEME_LOCAL = "current_theme_local"
+    private val themePink = R.style.PinkTheme
+    private val themeIndigo = R.style.IndigoTheme
+
+    private lateinit var parentActivity: MainActivity
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        parentActivity = requireActivity() as MainActivity
+    }
+
     private var _binding: FragmentSettingsBinding? = null
     private val binding get() = _binding!!
 
@@ -24,13 +38,33 @@ class SettingsFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentSettingsBinding.inflate(inflater, container, false)
+        val context: Context = ContextThemeWrapper(activity, getRealStyleLocal(getCurrentThemeLocal()))
+        val localInflater = inflater.cloneInContext(context)
+        _binding = FragmentSettingsBinding.inflate(localInflater)
         return binding.root
     }
 
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        when(getCurrentThemeLocal()) {
+            themePink -> binding.settingsChipPink.isChecked = true
+            themeIndigo -> binding.settingsChipIndigo.isChecked = true
+        }
+        binding.settingsChipPink.setOnClickListener {
+            setCurrentThemeLocal(themePink)
+            requireActivity().supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.container, SettingsFragment.newInstance())
+                .commit()
+        }
+        binding.settingsChipIndigo.setOnClickListener {
+            setCurrentThemeLocal(themeIndigo)
+            requireActivity().supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.container, SettingsFragment.newInstance())
+                .commit()
+        }
 
     }
 
@@ -42,4 +76,28 @@ class SettingsFragment : Fragment() {
     companion object {
         fun newInstance() = SettingsFragment()
     }
+
+
+    private fun setCurrentThemeLocal(currentTheme: Int){
+        val sharedPreferences =
+            requireActivity().getSharedPreferences(KEY_SP_LOCAL, AppCompatActivity.MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        editor.putInt(KEY_CURRENT_THEME_LOCAL, currentTheme)
+        editor.apply()
+    }
+
+    private fun getCurrentThemeLocal(): Int {
+        val sharedPreferences =
+            requireActivity().getSharedPreferences(KEY_SP_LOCAL, AppCompatActivity.MODE_PRIVATE)
+        return sharedPreferences.getInt(KEY_CURRENT_THEME_LOCAL, -1)
+    }
+
+    private fun getRealStyleLocal(currentTheme: Int): Int {
+        return when(currentTheme) {
+            themePink -> R.style.PinkTheme
+            themeIndigo -> R.style.IndigoTheme
+            else -> 0
+        }
+    }
+
 }
