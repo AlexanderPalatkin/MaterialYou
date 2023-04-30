577719245d0c9add904cb74fefb4c891cba891c0
AlexanderPalatkin <palatkin.alexander@gmail.com>
2023-04-30 16:10:09 +0400

    Урок 3. Добавил во ViewPagerActivity TabLayout для переключения фрагментов ViewPager. Сделал для табов кастомные иконки

--- app/src/main/java/com/example/materialyou/ui/viewpager/ViewPagerActivity.kt
+++ app/src/main/java/com/example/materialyou/ui/viewpager/ViewPagerActivity.kt
@@ -1,7 +1,9 @@
 package com.example.materialyou.ui.viewpager
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import androidx.appcompat.app.AppCompatActivity
+import com.example.materialyou.R
 import com.example.materialyou.databinding.ActivityViewPagerBinding
 
 class ViewPagerActivity : AppCompatActivity() {
@@ -11,5 +13,28 @@ class ViewPagerActivity : AppCompatActivity() {
         binding = ActivityViewPagerBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.viewPager.adapter = ViewPagerAdapter(supportFragmentManager)
+        binding.tabLayout.setupWithViewPager(binding.viewPager)
+
+        setCustomTabs()
+
+    }
+
+    private fun setCustomTabs() {
+        val layoutInflater = LayoutInflater.from(this)
+        with(binding.tabLayout) {
+            getTabAt(EARTH)?.customView =
+                layoutInflater.inflate(R.layout.activity_view_pager_custom_tab_earth, null)
+            getTabAt(MARS)?.customView =
+                layoutInflater.inflate(R.layout.activity_view_pager_custom_tab_mars, null)
+            getTabAt(WEATHER)?.customView =
+                layoutInflater.inflate(R.layout.activity_view_pager_custom_tab_weather, null)
+        }
+
+    }
+
+    companion object {
+        private const val EARTH = 0
+        private const val MARS = 1
+        private const val WEATHER = 2
     }
 }
